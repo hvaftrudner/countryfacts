c7a265ce784f9a9bf6547d2d269702f0aae81724
hvaftrudner <krill_eriksson@hotmail.com>
2020-10-15 22:34:37 +0200

    Initial Commit

---
+++ countryfacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  countryfacts
+//
+//  Created by Kristoffer Eriksson on 2020-10-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
